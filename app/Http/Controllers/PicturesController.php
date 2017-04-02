acd53bfcbb7d2e6cb39e58cf68398ae25e6c688f
taoyu <1255803549@qq.com>
2017-04-02 16:38:03 +0800

    添加ajax上传图片借口

--- app/Http/Controllers/PicturesController.php
+++ app/Http/Controllers/PicturesController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use PictureManager;
+use Ty666\PictureManager\Exception\UploadException;
 
 class PicturesController extends Controller
 {
     public function show($img_id, $size, $suffix)
     {
         return PictureManager::init($img_id, $size, $suffix)->show();
+    }
+
+    public function upload(Request $request)
+    {
+
+        try{
+            $picture = PictureManager::upload($request->file($request->get('picture_key', 'file')));
+        }catch (UploadException $e){
+            return $this->response->error('图片上传失败');
+        }
+
+        return ['picture' => $picture];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
-
+Route::get('/', function (){
+    return 'hello world!';
+});
 Route::get('pic/{img_id}_{size}_{suffix}', 'PicturesController@show')->name('image');
-
+Route::post('ajax_upload_image', 'PicturesController@upload');
