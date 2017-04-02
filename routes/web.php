0fcaa11713fe4f968a6507be1c952c6cc71c5dea
taoyu <1255803549@qq.com>
2017-04-02 17:55:23 +0800

    image->picture

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ Route::get('/', function (){
     return 'hello world!';
 });
 Route::get('pic/{img_id}_{size}_{suffix}', 'PicturesController@show')->name('image');
-Route::post('ajax_upload_image', 'PicturesController@upload');
+Route::post('ajax_upload_picture', 'PicturesController@upload');
