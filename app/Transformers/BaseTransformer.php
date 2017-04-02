93f8f6f54d35aea0ab6d0ff1c900845f0eee4027
taoyu <1255803549@qq.com>
2017-04-02 19:57:17 +0800

    取消将null转化为空字符串

--- app/Transformers/BaseTransformer.php
+++ app/Transformers/BaseTransformer.php
@@ -9,19 +9,7 @@ abstract class BaseTransformer extends TransformerAbstract
 {
     public function transform(Model $model)
     {
-        $data = $this->transformData($model);
-        // 转换 null 字段为空字符串
-        foreach (array_keys($data) as $key) {
-            if (! isset($data[$key])) {
-                $data[$key] = '';
-                continue;
-            }
-            if (is_null($data[$key])) {
-                $data[$key] = '';
-            }
-        }
-
-        return $data;
+        return $this->transformData($model);
     }
 
     abstract public function transformData($model);
