9575b55eb6b0f72971b74458ddd44a892b0f51bf
taoyu <1255803549@qq.com>
2017-04-11 17:32:53 +0800

    添加published_at

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -45,14 +45,12 @@ class PostsTableSeeder extends Seeder
             [
                 'post_id' => 1,
                 'content' => '这是第一篇文章',
-                'published_at' => \Carbon\Carbon::now(),
                 'created_at' => \Carbon\Carbon::now(),
                 'updated_at' => \Carbon\Carbon::now()
             ],
             [
                 'post_id' => 2,
                 'content' => '这是第二篇文章11111111',
-                'published_at' => \Carbon\Carbon::now(),
                 'created_at' => \Carbon\Carbon::now(),
                 'updated_at' => \Carbon\Carbon::now()
             ]
