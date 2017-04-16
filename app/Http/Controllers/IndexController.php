cbc2056abb2a76cf17185cf1f455429c6f45462d
taoyu <1255803549@qq.com>
2017-04-16 18:16:26 +0800

    添加分页

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Entities\Category;
 use App\Entities\Post;
+use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
@@ -13,7 +14,7 @@ class IndexController extends Controller
         return theme_view('index');
     }
 
-    public function postList($cateSlug)
+    public function postList($cateSlug, Request $request)
     {
         $currentCategory = Category::findBySlug($cateSlug);
 
@@ -25,8 +26,15 @@ class IndexController extends Controller
         $topCategory->load(['children' => function ($query) {
             $query->nav();
         }]);
-        return theme_view($currentCategory->list_template, ['topCategory' => $topCategory, 'currentCategory' => $currentCategory]);
+        $postList = $currentCategory->postList()->paginate($this->perPage());
+        $postList->appends($request->all());
+        return theme_view($currentCategory->list_template, [
+            'topCategory' => $topCategory,
+            'currentCategory' => $currentCategory,
+            'postList' => $postList
+        ]);
     }
+
 
     public function content(Post $post)
     {
