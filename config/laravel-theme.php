c5a6193a0907e157c6006a653aa576cd24ae3b89
taoyu <1255803549@qq.com>
2017-05-01 00:41:50 +0800

    更新 ty666/laravel-theme

--- config/laravel-theme.php
+++ config/laravel-theme.php
@@ -3,8 +3,8 @@
 return [
     'theme_path' => base_path('themes'),
     'config_file_name' => 'config.json',
-    'default_theme' => 'default',
-    'static_folder' => 'static',
     'public_theme_folder' => 'theme',
-    'screenshot_name' => 'screenshot'
+    'default_theme' => 'default',
+    'default_static_folder' => 'static',
+    'default_screenshot_name' => 'screenshot'
 ];
