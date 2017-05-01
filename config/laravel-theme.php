6e3cea77bb8dc54a3d57ef5c3d33513e134bcfec
taoyu <1255803549@qq.com>
2017-05-01 14:37:58 +0800

    去除 public_themes_folder

--- config/laravel-theme.php
+++ config/laravel-theme.php
@@ -3,7 +3,6 @@
 return [
     'themes_path' => base_path('themes'),
     'config_file_name' => 'config.json',
-    'public_themes_folder' => 'themes',
     'default_theme' => 'default',
     'default_screenshot_name' => 'screenshot',
     'screenshot_route' => [
