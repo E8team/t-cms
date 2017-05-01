90f4aff05b23591ef2b8187186a94642c7a6f427
taowei <t888y@qq.com>
2017-05-01 21:05:22 +0800

    添加initFunction

--- themes/default/assets/js/app.js
+++ themes/default/assets/js/app.js
@@ -1,1 +1,27 @@
 import './bootstrap.js'
+var original_title = document.title;
+var tcms = {
+    init: function () {
+        $(document).pjax('a:not(a[target="_blank"])', 'body', {
+            timeout: 1600,
+            maxCacheLength: 500
+        });
+        $(document).on('pjax:start', function() {
+            NProgress.start();
+        });
+        $(document).on('pjax:end', function() {
+            NProgress.done();
+        });
+        $(document).on('pjax:complete', function() {
+            original_title = document.title;
+            NProgress.done();
+            //self._resetTitle();
+        });
+        // Exclude links with a specific class
+        $(document).on("pjax:click", "a.no-pjax", false);
+    }
+}
+$(document).ready(function()
+{
+    tcms.init();
+});
