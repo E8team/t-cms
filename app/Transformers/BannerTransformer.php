ed56dea7e07fb7558bccbd9640f0225da8aa0869
taoyu <1255803549@qq.com>
2017-05-08 20:58:39 +0800

    ty

---
+++ app/Transformers/BannerTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Transformers;
+
+
+use App\Models\Banner;
+use League\Fractal\TransformerAbstract;
+
+class BannerTransformer extends TransformerAbstract
+{
+    public function transform(Banner $banner)
+    {
+        return [
+            'id' => $banner->id,
+            'url' => $banner->url,
+            'title' => $banner->title,
+            'order' => $banner->order,
+            'created_at' => $banner->created_at->toDateTimeString(),
+            'updated_at' => $banner->updated_at->toDateTimeString()
+        ];
+    }
+}
