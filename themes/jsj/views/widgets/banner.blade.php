4524cf7dc17032d4ede334939d9916ee2f3d6ca4
tw <t888y@qq.com>
2017-05-30 01:50:31 +0800

    添加链接

--- themes/jsj/views/widgets/banner.blade.php
+++ themes/jsj/views/widgets/banner.blade.php
@@ -38,19 +38,27 @@
 @endpush
 <section id="banner" class="slider">
     <div>
-        <img src="http://www.pku.edu.cn/images/content/2017-05/20170524123828405775.jpg">
+        <a href="#" target="#">
+            <img src="http://www.pku.edu.cn/images/content/2017-05/20170524123828405775.jpg">
+        </a>
         <p class="text">百廿北大 因你而新：北大2017年校园开放日暨“引领未来”本科生招生咨询会举行0</p>
     </div>
     <div>
-        <img src="http://www.pku.edu.cn/images/content/2017-05/20170514192853790577.jpg">
+        <a href="#" target="#">
+            <img src="http://www.pku.edu.cn/images/content/2017-05/20170514192853790577.jpg">
+        </a>
         <p class="text">百廿北大 因你而新：北大2017年校园开放日暨“引领未来”本科生招生咨询会举行1</p>
     </div>
     <div>
-        <img src="http://www.pku.edu.cn/images/content/2017-05/20170529133458089409.jpg">
+        <a href="#">
+            <img src="http://www.pku.edu.cn/images/content/2017-05/20170529133458089409.jpg">
+        </a>
         <p class="text">百廿北大 因你而新：北大2017年校园开放日暨“引领未来”本科生招生咨询会举行2</p>
     </div>
     <div>
-        <img src="http://www.pku.edu.cn/images/content/2017-05/20170522101204651052.jpg">
+        <a href="#">
+            <img src="http://www.pku.edu.cn/images/content/2017-05/20170522101204651052.jpg">
+        </a>
         <p class="text">百廿北大 因你而新：北大2017年校园开放日暨“引领未来”本科生招生咨询会举行3</p>
     </div>
 </section>
